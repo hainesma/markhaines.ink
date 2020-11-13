8cdd755d4382b488030e2043e3ce487b1bf7eb06
hainesma <hainesma@gmail.com>
2020-11-13 16:59:44 -0500

    Triangle

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,21 +122,21 @@
 		$tiles.each(function() {
 
 			var $this = $(this),
-				$image = $this.find('.image'), $img = $image.find('img'),
+			//	$image = $this.find('.image'), $img = $image.find('img'),
 				$link = $this.find('.link'),
 				x;
 
 			// Image.
 
 				// Set image.
-					$this.css('background-image', 'url(' + $img.attr('src') + ')');
+				//	$this.css('background-image', 'url(' + $img.attr('src') + ')');
 
 				// Set position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
+				//	if (x = $img.data('position'))
+				//		$image.css('background-position', x);
 
 				// Hide original.
-					$image.hide();
+				//	$image.hide();
 
 			// Link.
 				if ($link.length > 0) {
